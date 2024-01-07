5d355b2f4e57f2b4e807132547c4d3bf2789765a
Fabio Luiz Régis <88636618+fabioluizregis@users.noreply.github.com>
2024-01-07 12:01:59 +0000

    Adicionando o lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-function Home()
-{
-    return <h1>Nunca deixe de acreditar na SUA capacidade criativa e o quão longe isto pode te levar!</h1>
-
+function Home() {
+  return (
+    <h1>
+      Nunca deixe de acreditar na SUA capacidade criativa e o quão longe isto
+      pode te levar!
+    </h1>
+  );
 }
 
 export default Home;
